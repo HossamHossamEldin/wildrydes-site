e1b3c9a656d9693a1b61a878ef0a3f926c855967
HossamHossamEldin <168969441+HossamHossamEldin@users.noreply.github.com>
2025-08-01 19:03:24 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_mKzVbDMVY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2pf7rmj0gt0png5437s34cajcc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {

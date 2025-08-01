15dff01efb51f729d244b8ddc2c6d0434c18d540
HossamHossamEldin <168969441+HossamHossamEldin@users.noreply.github.com>
2025-08-01 20:26:04 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_HEkqaiPut', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2pf7rmj0gt0png5437s34cajcc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://main.dg7gafky0c0lt.amplifyapp.com/',
     }
 };
